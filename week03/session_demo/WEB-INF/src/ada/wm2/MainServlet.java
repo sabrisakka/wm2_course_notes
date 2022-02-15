f22b2296350dbf704c83e5c1f0dbdfa33434f251
hajimins <minura.hajisoy@gmail.com>
2022-02-15 18:05:39 +0400

    just

--- week03/session_demo/WEB-INF/src/ada/wm2/MainServlet.java
+++ week03/session_demo/WEB-INF/src/ada/wm2/MainServlet.java
@@ -3,6 +3,7 @@ package ada.wm2;
 import javax.servlet.*;  
 import javax.servlet.http.*;
 import java.util.*;
+import java.io.*;
 
 public class MainServlet extends HttpServlet{
 
@@ -16,7 +17,48 @@ public class MainServlet extends HttpServlet{
 		users.add("Sveta");
 	}
 
-	
+	public void doPost(HttpServletRequest req, HttpServletResponse res){
+		/*try{
+
+			PrintWriter pw = res.getWriter();
 
 
+
+			pw.write("<html><body>Welcome!</body></html>");
+
+		}catch(IOException ex){
+
+			System.out.println(ex);
+
+		}*/
+
+		//admin: admin
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+
+		if("admin".equals(username) && "admin".equals(password)){
+			//successful login
+			//forward to the users.jsp
+
+			HttpSession session = req.getSession();
+			session.setAttribute("current_user", username);
+			session.setAttribute("user_list", users);
+
+			try{
+			RequestDispatcher disp = req.getRequestDispatcher("user.jsp");
+			disp.forward(req, res);
+			}catch(Exception ex){
+				System.out.println("Forward failed: " + ex);
+			}
+		}else {
+			//login failed
+			//redirect to login.html
+
+			try{
+				res.sendRedirect("index.html");
+			}catch(Exception ex){
+				System.out.println("Forward failed: " + ex);
+			}
+		}
+	}
 }
